f34e1fdbe6bebbaf45a4b6d0a0e772829df095b9
Ayush Rawat <ayushrawat3189@gmail.com>
2022-10-05 21:53:15 +0530

    chapter one complete i think

--- chapter-one/src/com/ayush/Main.java
+++ chapter-one/src/com/ayush/Main.java
@@ -2,6 +2,26 @@ package com.ayush;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Student student1 = new Student();
+
+        System.out.println(student1.rno);
+        System.out.println(student1.name);
+        System.out.println(student1.marks);
     }
+}
+class Student{
+    int rno;
+    String name;
+    float marks;
+
+    Student(){
+        this (0,null,0);
+    }
+
+    Student(int rno,String name, float marks){
+        this.rno = 9;
+        this.name = "Ayush";
+        this.marks = 90.0f;
+    }
+
 }
